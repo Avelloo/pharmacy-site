f676098127287024341c7545375674649bfec1e3
Avello <aloobon@gmail.com>
2022-05-04 20:09:29 +0300

    post login handler

---
+++ backend/utils.js
@@ -0,0 +1,14 @@
+import jws from "jsonwebtoken";
+
+export const generateToken = (user) => {
+  return jws.sign(
+    {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+    process.env.JWT_SECRET || 'somethingsecret',
+    { expiresIn: "30d" }
+  );
+};
