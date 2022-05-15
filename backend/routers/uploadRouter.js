9919ea3792fd50e8a664651eb0ea5bb408f37ca0
Avello <aloobon@gmail.com>
2022-05-15 20:48:43 +0300

    heroku fix >:(

--- backend/routers/uploadRouter.js
+++ backend/routers/uploadRouter.js
@@ -6,7 +6,7 @@ const uploadRouter = express.Router();
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, './frontend/build/images');
+    cb(null, '../frontend/build/images');
   },
   filename(req, file, cb) {
     cb(null, `${Date.now()}.png`);
@@ -18,7 +18,7 @@ const upload = multer({ storage });
 uploadRouter.post('/', isAuth, upload.single('image'), (req, res) => {
   const response = `/${req.file.path}`;
   const replace = '/frontend\\build\\';
-  res.send('/' + response.replace(replace,''));
+  res.send(response.replace(replace,''));
 });
 
 export default uploadRouter;
