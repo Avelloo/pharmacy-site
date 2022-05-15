9b3b0af8839e231269146850006fb19b7df05098
Avello <aloobon@gmail.com>
2022-05-15 18:36:39 +0300

    heroku
    heroku fix
    
    heroku fix 2
    
    heroku fix3
    
    herokufix4
    
    heroku fix5
    
    heroku img test
    
    heroku img fix 2
    
    heroku img
    
    heroku fix =)
    
    heroku fix img =(

--- backend/server.js
+++ backend/server.js
@@ -6,16 +6,23 @@ import userRouter from "./routers/userRouter.js";
 import dotenv from "dotenv";
 import orderRouter from "./routers/orderRouter.js";
 import uploadRouter from './routers/uploadRouter.js';
+import seedRouter from "./routers/seedRouter.js";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/pharmacyDB')
-
+mongoose.connect(process.env.MONGO_DB_URL)
+        .then(() =>{
+            console.log('connected to db')
+        })
+        .catch((err) => {
+            console.log(err.message);
+        });
 
 app.use('/api/uploads', uploadRouter);
 app.use('/api/users', userRouter);
+app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter);
 app.get('/api/config/paypal', (req, res) => {
@@ -32,6 +39,8 @@ app.get('*', (req, res) =>
 // app.get('/', (req, res) => {
 //   res.send('Server is ready');
 // });
+
+
 
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message });
