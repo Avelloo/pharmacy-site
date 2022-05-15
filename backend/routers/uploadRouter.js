aeac71ad8371e7527547cf6b0ae28e950b899164
Avello <aloobon@gmail.com>
2022-05-15 18:36:39 +0300

    heroku
    heroku fix
    
    heroku fix 2
    
    heroku fix3
    
    herokufix4
    
    heroku fix5
    
    heroku img test
    
    heroku img fix 2
    
    heroku img
    
    heroku fix =)
    
    heroku fix img =(

--- backend/routers/uploadRouter.js
+++ backend/routers/uploadRouter.js
@@ -6,7 +6,7 @@ const uploadRouter = express.Router();
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, './frontend/public/images');
+    cb(null, './frontend/build/images');
   },
   filename(req, file, cb) {
     cb(null, `${Date.now()}.png`);
@@ -17,7 +17,7 @@ const upload = multer({ storage });
 
 uploadRouter.post('/', isAuth, upload.single('image'), (req, res) => {
   const response = `/${req.file.path}`;
-  const replace = '/frontend\\public\\';
+  const replace = '/frontend\\build\\';
   res.send('/' + response.replace(replace,''));
 });
 
