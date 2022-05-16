27264561280c01b72b5b27f06801cedf9737328a
Avello <aloobon@gmail.com>
2022-05-16 16:34:17 +0300

    yandex s3 integration

--- backend/routers/uploadRouter.js
+++ backend/routers/uploadRouter.js
@@ -1,24 +1,48 @@
-import multer from 'multer';
-import express from 'express';
-import { isAuth } from '../utils.js';
+import multer from "multer";
+import express from "express";
+import { isAuth } from "../utils.js";
+import path from "path";
+import EasyYandexS3 from "easy-yandex-s3";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+var s3 = new EasyYandexS3({
+  auth: {
+    accessKeyId: process.env.YANDEX_IDKEY,
+    secretAccessKey: process.env.YANDEX_SECRETKEY,
+  },
+  Bucket: "web-apteka-storage",
+  debug: true,
+});
 
 const uploadRouter = express.Router();
 
+async function start(req) {
+  var upload = await s3.Upload(
+    {
+      path: path.resolve(__dirname, req),
+    },
+    "/test/"
+  );
+  console.log(upload);
+}
+
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, './frontend/build/images');
+    cb(null, "./frontend/build/images");
   },
   filename(req, file, cb) {
     cb(null, `${Date.now()}.png`);
   },
 });
- //as
-const upload = multer({ storage });
 
-uploadRouter.post('/', isAuth, upload.single('image'), (req, res) => {
-  const response = `/${req.file.path}`;
-  const replace = '/frontend/build';
-  res.send(response.replace(replace,''));
-});
+uploadRouter.post("/", isAuth), (req, res) => {
+
+  start(req);
+  res.send(response.replace(replace, ""));
+};
 
 export default uploadRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,52 +1,56 @@
 import express from "express";
 import mongoose from "mongoose";
 import productRouter from "./routers/productRouter.js";
-import path from 'path';
+import path from "path";
 import userRouter from "./routers/userRouter.js";
 import dotenv from "dotenv";
 import orderRouter from "./routers/orderRouter.js";
-import uploadRouter from './routers/uploadRouter.js';
+import uploadRouter from "./routers/uploadRouter.js";
 import seedRouter from "./routers/seedRouter.js";
+
+
 
 dotenv.config();
 const app = express();
+
+
+
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-mongoose.connect(process.env.MONGO_DB_URL)
-        .then(() =>{
-            console.log('connected to db')
-        })
-        .catch((err) => {
-            console.log(err.message);
-        });
+app.use(express.urlencoded({ extended: true }));
+mongoose
+  .connect(process.env.MONGO_DB_URL)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
-app.use('/api/uploads', uploadRouter);
-app.use('/api/users', userRouter);
-app.use('/api/seed', seedRouter);
-app.use('/api/products', productRouter);
-app.use('/api/orders', orderRouter);
-app.get('/api/config/paypal', (req, res) => {
-    res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
-})
+app.use("/api/uploads", uploadRouter);
+app.use("/api/users", userRouter);
+app.use("/api/seed", seedRouter);
+app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+});
 
-const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+//const __dirname = path.resolve();
+//app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
 );
 // app.get('/', (req, res) => {
 //   res.send('Server is ready');
 // });
 
-
-
 app.use((err, req, res, next) => {
-    res.status(500).send({ message: err.message });
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Serve at http://localhost:${port}`);
+  console.log(`Serve at http://localhost:${port}`);
 });
