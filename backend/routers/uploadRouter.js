c7896014334a99ede15b66c458d746b756bb66ff
Avello <aloobon@gmail.com>
2022-05-16 17:02:01 +0300

    yandex s3 test

--- backend/routers/uploadRouter.js
+++ backend/routers/uploadRouter.js
@@ -3,11 +3,7 @@ import express from "express";
 import { isAuth } from "../utils.js";
 import path from "path";
 import EasyYandexS3 from "easy-yandex-s3";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 var s3 = new EasyYandexS3({
   auth: {
@@ -20,29 +16,13 @@ var s3 = new EasyYandexS3({
 
 const uploadRouter = express.Router();
 
-async function start(req) {
-  var upload = await s3.Upload(
-    {
-      path: path.resolve(__dirname, req),
-    },
-    "/test/"
-  );
-  console.log(upload);
-}
 
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, "./frontend/build/images");
-  },
-  filename(req, file, cb) {
-    cb(null, `${Date.now()}.png`);
-  },
-});
+app.use(multer().any());
 
-uploadRouter.post("/", isAuth, (req, res) => {
-
-  start(req);
-  res.send(response.replace(replace, ""));
+uploadRouter.post("/", isAuth, async(req, res) => {
+  let buffer = req.files[0].buffer; // Буфер загруженного файла
+  var upload = await s3.Upload({buffer}, '/files/'); // Загрузка в бакет
+  res.send(upload); // Ответ сервера - ответ от Yandex Object Storage
 });
 
 export default uploadRouter;
