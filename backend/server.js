6cdbf3cb6471b00a563b06b979924975d7d8482c
Avello <62185163+Avelloo@users.noreply.github.com>
2023-05-18 07:14:50 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -60,7 +60,7 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 const httpServer = http.Server(app);
 const io = new Server(httpServer, { cors: { origin: "*" } });
 const users = [];
