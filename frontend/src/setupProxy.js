55812841e70795d41479aece50cc40e5461bf9f1
Avello <62185163+Avelloo@users.noreply.github.com>
2023-05-18 08:04:39 +0300

    Update setupProxy.js

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: 'https://web-apteka-backend.onrender.com:10000',
       changeOrigin: true,
     })
   );
